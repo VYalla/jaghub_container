61bfaec7408ba67107e07959bc10b0b5b819746c
VYalla <91841638+VYalla@users.noreply.github.com>
2022-12-28 16:14:06 -0800

    initial commit

---
+++ lib/tabview.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:mad2_browsepage/browse_page.dart';
+
+class TabView extends StatefulWidget {
+  const TabView({Key? key}) : super(key: key);
+
+  @override
+  State<TabView> createState() => _TabViewState();
+}
+
+class _TabViewState extends State<TabView> {
+  int currentIndex = 0;
+
+  final screens = [
+    BrowsePage(),
+    Text("H"),
+    Text("H"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFFF5F5FA),
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people),
+            label: 'Groups',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+        ],
+      ),
+    );
+  }
+}
